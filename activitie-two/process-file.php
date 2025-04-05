984fe3bfbf2b2cdba971e24f14772c216ff7124d
Nar Mena <narciris.tech@gmail.com>
2025-04-04 20:10:30 -0500

    update changes

--- activitie-two/process-file.php
+++ activitie-two/process-file.php
@@ -5,12 +5,18 @@ $tpm_file = $_FILES['file']['tmp_name'];
 $name = basename($_FILES['file']['name']);
 $extension = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
 
+if(empty($directory)){
+    $extension = 'otros';
+}
+
 $directory= './file' . $extension;
 if(!is_dir($directory)){
     mkdir($directory, 0777, true);
 }
 
-$loadFile = move_uploaded_file($tpm_file,"$directory/$name");
+$uniqueName = uniqid() . '.' .$name;
+
+$loadFile = move_uploaded_file($tpm_file,"$directory/$uniqueName");
 if($loadFile){
     echo 'Archivo Cargado';
 } else{
